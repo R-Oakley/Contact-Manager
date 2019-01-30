e9cd1229e0503d1ab633ecc9d6d46db688599808
Ryan Oakley <yelkao85@gmail.com>
2019-01-29 21:35:40 -0400

    Changed the form to use context for state and added Input component

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+import uuid from 'uuid';
 
 class AddContact extends Component {
     state = {
@@ -7,60 +10,81 @@ class AddContact extends Component {
         phone: ''
     };
 
-    onSubmit = e => {
+    onSubmit = (dispatch, e) => {
         e.preventDefault();
+
+        const { name, email, phone } = this.state;
+
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            phone
+        };
+
+        dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+        // Clear the state
+        this.state.setState({
+            name: '',
+            email: '',
+            phone: ''
+        });
     };
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
         const { name, email, phone } = this.state;
+
         return (
-            <div className="card mb-3">
-                <div className="card-header">Add Contact</div>
-                <div className="card-body">
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                className="form-control form-control-lg"
-                                placeholder="Enter Name..."
-                                value={name}
-                                onChange={this.onChange}
-                            />
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className="card mb-3">
+                            <div className="card-header">Add Contact</div>
+                            <div className="card-body">
+                                <form
+                                    onSubmit={this.onSubmit.bind(
+                                        this,
+                                        dispatch
+                                    )}
+                                >
+                                    <TextInputGroup
+                                        label="Name"
+                                        name="name"
+                                        placeholder="Enter Name..."
+                                        value={name}
+                                        onChange={this.onChange}
+                                    />
+                                    <TextInputGroup
+                                        label="Email"
+                                        name="email"
+                                        type="email"
+                                        placeholder="Enter Email..."
+                                        value={email}
+                                        onChange={this.onChange}
+                                    />
+                                    <TextInputGroup
+                                        label="Phone"
+                                        name="phone"
+                                        placeholder="Enter Phone..."
+                                        value={phone}
+                                        onChange={this.onChange}
+                                    />
+
+                                    <input
+                                        type="submit"
+                                        value="Add Contact"
+                                        className="btn btn-light btn-block"
+                                    />
+                                </form>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                className="form-control form-control-lg"
-                                placeholder="Enter Email..."
-                                value={email}
-                                onChange={this.onChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phone">Phone</label>
-                            <input
-                                type="text"
-                                name="phone"
-                                className="form-control form-control-lg"
-                                placeholder="Enter Phone..."
-                                value={phone}
-                                onChange={this.onChange}
-                            />
-                        </div>
-                        <input
-                            type="submit"
-                            value="Add Contact"
-                            className="btn btn-light btn-block"
-                        />
-                    </form>
-                </div>
-            </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
