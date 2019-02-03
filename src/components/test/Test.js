14e10ce071cc5b834c0e95cef3d3e45b9c7adfe8
Ryan Oakley <yelkao85@gmail.com>
2019-02-03 11:09:17 -0400

    example of fetch in test and axios in contacts

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,47 +2,58 @@ import React, { Component } from 'react';
 
 class Test extends Component {
     state = {
-        test: 'test'
+        title: '',
+        body: ''
     };
 
     //! Lifecycle methods
 
     // After its Loaded, where Ajax would normally go
     componentDidMount() {
-        console.log('ComponentDidMount...');
+        // console.log('ComponentDidMount...');
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+            .then(response => response.json())
+            .then(data =>
+                this.setState({
+                    title: data.title,
+                    body: data.body
+                })
+            );
     }
     // Before its loaded - Being Deprecated
-    componentWillMount() {
-        console.log('ComponentWillMount...');
-    }
+    // componentWillMount() {
+    //     console.log('ComponentWillMount...');
+    // }
     // When its updated IE The state has changed
-    componentDidUpdate() {
-        console.log('ComponentDidUpdate...');
-    }
+    // componentDidUpdate() {
+    //     console.log('ComponentDidUpdate...');
+    // }
 
     // Being Deprecated
-    componentWillUpdate() {
-        console.log('ComponentWillUpdate...');
-    }
+    // componentWillUpdate() {
+    //     console.log('ComponentWillUpdate...');
+    // }
 
     // Used with redux - Being Deprecated
-    componentWillReceiveProps(nextProps, nextState) {
-        console.log('ComponentWillReceiveProps...');
-    }
+    // componentWillReceiveProps(nextProps, nextState) {
+    //     console.log('ComponentWillReceiveProps...');
+    // }
 
     // return a state or null
-    static getDerivedStateFromProps(nextProps, prevState) {
-        return null;
-    }
+    // static getDerivedStateFromProps(nextProps, prevState) {
+    //     return null;
+    // }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log('getSnapShotBeforeUpdate...');
-    }
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     console.log('getSnapShotBeforeUpdate...');
+    // }
 
     render() {
+        const { title, body } = this.state;
         return (
             <div>
-                <h1>Test Component</h1>
+                <h1>{title}</h1>
+                <p>{body}</p>
             </div>
         );
     }
