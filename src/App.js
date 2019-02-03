10a67cd772718f8ed71f62ab1a559e5aa6a89337
Ryan Oakley <yelkao85@gmail.com>
2019-02-03 13:21:34 -0400

    added change to hashrouter so it works with githubpages and added script stuff needed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//! This HashRouter works with github pages
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
